576136b395732f384c5cceb6a4fc28949dfa2bb5
Mohamed-1628 <allaoua.mohamed1628@gmail.com>
2023-08-28 16:02:28 +0100

    open-admin

---
+++ app/Admin/Controllers/CardController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use OpenAdmin\Admin\Controllers\AdminController;
+use OpenAdmin\Admin\Form;
+use OpenAdmin\Admin\Grid;
+use OpenAdmin\Admin\Show;
+use \App\Models\Card;
+
+class CardController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Card';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Card());
+
+        $grid->column('id', __('Id'));
+        $grid->column('name', __('Name'));
+        $grid->column('image', __('Image'));
+        $grid->column('link', __('Link'));
+        $grid->column('btn', __('Btn'));
+        $grid->column('description', __('Description'));
+        $grid->column('order', __('Order'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Card::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('name', __('Name'));
+        $show->field('image', __('Image'));
+        $show->field('link', __('Link'));
+        $show->field('btn', __('Btn'));
+        $show->field('description', __('Description'));
+        $show->field('order', __('Order'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Card());
+
+        $form->text('name', __('Name'));
+        $form->image('image', __('Image'));
+        $form->url('link', __('Link'));
+        $form->text('btn', __('Btn'));
+        $form->text('description', __('Description'));
+        $form->number('order', __('Order'));
+
+        return $form;
+    }
+}
